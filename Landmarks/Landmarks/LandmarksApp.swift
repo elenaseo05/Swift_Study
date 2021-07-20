452ba360981e72bd56866f4dfd3a35bba030400a
elenaseo05 <elenaseo05@gmail.com>
2021-07-20 23:41:36 +0900

    ADD swift tutorials project create

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Elena on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
