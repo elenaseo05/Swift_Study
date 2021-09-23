9bf1609566c190e3011925c81478aa25e1e0ff2d
elenaseo05 <elenaseo05@gmail.com>
2021-09-23 23:14:21 +0900

    Update LedBoard

---
+++ LedBoard/LedBoard/SettingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingViewController.swift
+//  LedBoard
+//
+//  Created by Elena on 2021/09/23.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+
+}
