8243c4a20d846756ff9380f124d614b6161b908b
elenaseo05 <elenaseo05@gmail.com>
2021-09-24 23:39:41 +0900

    btn

--- LedBoard/LedBoard/SettingViewController.swift
+++ LedBoard/LedBoard/SettingViewController.swift
@@ -8,13 +8,40 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-
+    // text
+    @IBOutlet weak var textField: UITextField!
+    // text btn
+    @IBOutlet weak var yellowBtn: UIButton!
+    @IBOutlet weak var purpleBtn: UIButton!
+    @IBOutlet weak var greenBtn: UIButton!
+    // background
+    @IBOutlet weak var blackBtn: UIButton!
+    @IBOutlet weak var buleBtn: UIButton!
+    @IBOutlet weak var orangeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    @IBAction func tabTextColorBtn(_ sender: UIButton) {
         
         
     }
+    
 
-
+    @IBAction func tabBackgroundColorBtn(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func tabSaveBtn(_ sender: UIButton) {
+        
+    }
+    
+    private func changeTextColor(color: UIColor ) {
+        self.yellowBtn.alpha = color == UIColor.yellow ? 1 : 0.2
+        self.purpleBtn.alpha = color == UIColor.purple ? 1 : 0.2
+        self.greenBtn.alpha = color == UIColor.green ? 1 : 0.2
+    }
+    
 }
