c46c3379dad62224c0fa0ce002acdee7f78f6bba
elenaseo05 <elenaseo05@gmail.com>
2021-10-12 20:34:41 +0900

    Button connection

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberOutputLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tabNumberButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabClearButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabDotButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabDivideButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabMultiplyButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabSubtractButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabAddButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tabEqualButton(_ sender: UIButton) {
+    }
+    
 }
 
