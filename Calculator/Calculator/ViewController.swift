19dda36d2c46755584f1f7bdfbc43e3d827354b7
elenaseo05 <elenaseo05@gmail.com>
2021-10-14 22:19:21 +0900

    update

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func tabNumberButton(_ sender: UIButton) {
