da5754274aa5bac90de39eea135069a28b49409b
elenaseo05 <elenaseo05@gmail.com>
2021-10-26 13:20:39 +0900

    debugged add list

--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -8,18 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var tableView: UITableView!
     var tasks = [Task]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
     }
-
-    @IBAction func tabEditBtn(_ sender: UIBarButtonItem) { // Edit 버튼 클릭시
+  
+    @IBAction func tabEditBtn(_ sender: UIBarButtonItem) {
     }
     
-    @IBAction func tabAddBtn(_ sender: UIBarButtonItem) { // Add(+) 버튼 클릭시
+    
+    @IBAction func tabAddBtn(_ sender: UIBarButtonItem) {
+    // Add(+) 버튼 클릭시
         let alert = UIAlertController(title: "ADD Todo", message: nil, preferredStyle: .alert) // alert 창 띄우기
         
         let registerBtn = UIAlertAction(title: "ADD", style: .default, handler: { [weak self] _ in
@@ -37,14 +40,13 @@ class ViewController: UIViewController {
             textField.placeholder = "what are you going to do today"
         })
         self.present(alert, animated: true, completion: nil)
-        
-        
+    
     }
-    
-    
 }
 
 extension ViewController: UITableViewDataSource {
+    // extension : 기존에 존재하는 구조체, 클래스, 열거형에 새로운 기능 추가, 확장 가능
+    // 오버라이딩 불가
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.tasks.count
     }
