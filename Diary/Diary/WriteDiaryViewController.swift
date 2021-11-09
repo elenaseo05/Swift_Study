0d46b111b88456cceed0492e6d318837dfe3cfc8
elenaseo05 <elenaseo05@gmail.com>
2021-11-10 08:42:13 +0900

    update ui

---
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by Elena on 2021/11/09.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
