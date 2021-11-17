009ee270633eb703bae0619a09ec6a055c292108
elenaseo05 <elenaseo05@gmail.com>
2021-11-17 19:27:56 +0900

    Updata Diary UI

---
+++ Diary/Diary/DiaryDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by Elena on 2021/11/17.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
