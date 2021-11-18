d827832078d1ae5d5d33e1fb07b74448b2093685
elenaseo05 <elenaseo05@gmail.com>
2021-11-18 19:58:28 +0900

    Update UI border, datepicker

--- Diary/Diary/WriteDiaryViewController.swift
+++ Diary/Diary/WriteDiaryViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class WriteDiaryViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contantsTextView: UITextView!
+    @IBOutlet weak var dateTextFieid: UITextField!
+    
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
+    private let datepicker = UIDatePicker()
+    private var diaryDate: Date?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.configureContentsTextView()
         
     }
+    
+    
+    private func configureContentsTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        // RGB 0.0 ~ 1.0 => X / 255
+        //alpha 0.0 ~ 1.0
+        
+        self.contantsTextView.layer.borderColor = borderColor.cgColor // 색상
+        // layer 관련 색상은 cgColor로 설정
+        
+        self.contantsTextView.layer.borderWidth = 0.5 // 테두리 두께
+        self.contantsTextView.layer.cornerRadius = 5.0 // 둥굴게
+    }
+    
+    private func configureDatePicker() {
+        self.datepicker.datePickerMode = .date
+        self.datepicker.preferredDatePickerStyle = .wheels
+    }
+    
     
 
 }
